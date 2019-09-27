ecd96a89901bfe19701a31eba043021347ecbfe8
Yu Yu4 Yan <yanyu4@Lenovo.com>
2019-09-27 11:02:23 +0800

    continue

--- leguess/match/__init__.py
+++ leguess/match/__init__.py
@@ -8,11 +8,14 @@ def merge_match(match_res_list):
         return match_res_list[0]
     match_size = len(match_res_list[0])
     match_set = set()
+    match_list = []
     id = 0
-    while len(match_set) < match_size:
+    while len(match_list) < match_size:
         for match_res in match_res_list:
-            match_set.add(match_res[id])
-            if len(match_set) == match_size:
-                break
+            if match_res[id] not in match_set:
+                match_set.add(match_res[id])
+                match_list.append(match_res[id])
+                if len(match_list) == match_size:
+                    break
         id += 1
-    return list(match_set)
+    return match_list
