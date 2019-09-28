f691a818a06c1e493e41f2a97a38ec82fc1fa5df
Yu Yu4 Yan <yanyu4@Lenovo.com>
2019-09-29 07:17:57 +0800

    continue

--- leguess/__init__.py
+++
@@ -1,4 +0,0 @@
-from .db import *
-from .match import *
-from .rank import *
-from .eval import eval_match
--- leguess/match/__init__.py
+++ leguess/match/__init__.py
@@ -2,6 +2,7 @@ from .itembase_match import ItemBaseMatch
 from .modelbase_match import ModelBaseMatch
 
 
+# 合并多个召回结果
 def merge_match(match_res_list):
     assert len(match_res_list) > 0
     if len(match_res_list) == 1:
@@ -19,3 +20,19 @@ def merge_match(match_res_list):
                     break
         id += 1
     return match_list
+
+
+# 切分相似行为，以便编码成vector
+def split_near_act(act_list, l_skip, r_skip):
+    near_act_list = []
+    target_act_list = []
+    for i in range(len(act_list)):
+        near_acts = []
+        for j in range(max(0, i - l_skip), i):
+            near_acts.append(act_list[j])
+        for j in range(i+1, min(len(act_list), i + r_skip + 1)):
+            near_acts.append(act_list[j])
+        if len(near_acts) > 0:
+            target_act_list.append(act_list[i])
+            near_act_list.append(near_acts)
+    return target_act_list, near_act_list
