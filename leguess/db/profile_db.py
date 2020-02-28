4f0d96b82ce5f4756131aad435e06a06f288b630
Yu Yu4 Yan <yanyu4@Lenovo.com>
2020-02-28 12:14:19 +0800

    continue

--- leguess/db/profile_db.py
+++ leguess/db/profile_db.py
@@ -4,7 +4,7 @@ class UserProfileDB(object):
 
     def get_act_list(self, user_id):
         # 返回行为序列，频道序列，时间序列
-        return None, None, None
+        return None, None, None, None
 
     def get_tag_list(self, user_id):
         # 返回标签序列
@@ -27,7 +27,7 @@ class MemoryUserProfileDB(UserProfileDB):
         self.user_tag_dict = {}
 
     def get_act_list(self, user_id):
-        return self.user_act_dict.get(user_id, (None, None, None))
+        return self.user_act_dict.get(user_id, (None, None, None, None))
 
     def get_tag_list(self, user_id):
         return self.user_tag_dict.get(user_id, None)
@@ -35,26 +35,28 @@ class MemoryUserProfileDB(UserProfileDB):
     def refresh_tag_list(self, user_id, tag_list):
         self.user_tag_dict[user_id] = tag_list
 
-    def push_act(self, user_id, act_name, channel, timestamp):
+    def push_act(self, user_id, act_name, channel, org, timestamp):
         if self.act_filter is not None and self.act_filter(self.user_act_dict.get(user_id, []), act_name) is False:
             return False
         if user_id not in self.user_act_dict:
-            self.user_act_dict[user_id] = ([act_name], [channel], [timestamp])
+            self.user_act_dict[user_id] = ([act_name], [channel], [org], [timestamp])
         else:
             self.user_act_dict[user_id][0].append(act_name)
             self.user_act_dict[user_id][1].append(channel)
-            self.user_act_dict[user_id][2].append(timestamp)
+            self.user_act_dict[user_id][2].append(org)
+            self.user_act_dict[user_id][3].append(timestamp)
         return True
 
-    def refresh_act_list(self, user_id, act_list, channel_list, timestamp_list):
-        a_list, c_list, t_list = [], [], []
-        for a, c, t in zip(act_list, channel_list, timestamp_list):
+    def refresh_act_list(self, user_id, act_list, channel_list, org_list, timestamp_list):
+        a_list, c_list, o_list, t_list = [], [], [], []
+        for a, c, o, t in zip(act_list, channel_list, org_list, timestamp_list):
             if self.act_filter is not None and self.act_filter(a_list, a) is False:
                 continue
             a_list.append(a)
             c_list.append(c)
+            o_list.append(o)
             t_list.append(t)
-        self.user_act_dict[user_id] = (a_list, c_list, t_list)
+        self.user_act_dict[user_id] = (a_list, c_list, o_list, t_list)
 
 
 class RedisUserProfileDB(UserProfileDB):
--- leguess/eval/__init__.py
+++ leguess/eval/__init__.py
@@ -3,31 +3,54 @@ from .hr import HR
 import numpy as np
 
 
-def eval_recm(hdfs_db, user_profile_db, test_act_list_path, recm_call_back, top_list, pred_act_type="download", pred_channel="default", exclude_act_type_list=["show"], filter_item_set=None):
+def eval_recm(hdfs_db, user_profile_db, test_act_list_path, recm_call_back, top_list, pred_act_type="download", pred_channel="jingxuan", exclude_act_type_list=["show"], offline_time=1*60*60, filter_item_set=None):
     # 评估召回和排序的命中率---------------------------------------------------------------
-    match_hr = HR(top_list)
-    rank_hr = HR([top_list[0]])
+    offline_match_hr = HR(top_list)
+    offline_rank_hr = HR([top_list[0]])
+    online_match_hr = HR(top_list)
+    online_rank_hr = HR([top_list[0]])
+    default_match_hr = HR([top_list[0]])
 
     # 读取测试数据
-    def _eval_recm(user, act_list, channel_list, timestamp_list):
+    def _eval_recm(user, act_list, channel_list, org_list, timestamp_list):
         item_size = 0
         for act, channel in zip(act_list, channel_list):
-            if act.endswith(pred_act_type) and channel == pred_channel and (filter_item_set is None or act.split("@")[0] in filter_item_set):
+            if act.split('@')[1] == pred_act_type and channel == pred_channel and (filter_item_set is None or act.split("@")[0] in filter_item_set):
                 item_size += 1
-        for act, channel, timestamp in zip(act_list, channel_list, timestamp_list):
-            if act.endswith(pred_act_type) and channel == pred_channel and (filter_item_set is None or act.split("@")[0] in filter_item_set):
+        for act, channel, org, timestamp in zip(act_list, channel_list, org_list, timestamp_list):
+            if act.split('@')[1] == pred_act_type and channel == pred_channel and (filter_item_set is None or act.split("@")[0] in filter_item_set):
                 # 开始召回
-                act_list, channel_list, timestamp_list = user_profile_db.get_act_list(user)
+                act_list, channel_list, org_list, timestamp_list = user_profile_db.get_act_list(user)
                 tag_list = user_profile_db.get_tag_list(user)
-                match_item, rank_percent = recm_call_back(user, act_list, channel_list, timestamp_list, tag_list, timestamp)
-                match_hr.add_sample(match_item, act.split("@")[0], 1.0/item_size)
-                ids = np.argsort(-rank_percent)
-                rank_hr.add_sample(match_item[ids], act.split("@")[0], 1.0/item_size)
-            user_profile_db.push_act(user, act, channel, timestamp)
+
+                if len(act_list) == 0:
+                    match_item, rank_percent = recm_call_back(user, act_list, channel_list, org_list, timestamp_list,
+                                                              tag_list, timestamp, True)
+                    default_match_hr.add_sample(match_item, act.split("@")[0], 1.0/item_size)
+                else:
+                    if timestamp - timestamp_list[-1] > offline_time:
+                        match_item, rank_percent = recm_call_back(user, act_list, channel_list, org_list,
+                                                                  timestamp_list,
+                                                                  tag_list, timestamp, True)
+                        offline_match_hr.add_sample(match_item, act.split("@")[0], 1.0/item_size)
+                        ids = np.argsort(-rank_percent)
+                        offline_rank_hr.add_sample(match_item[ids], act.split("@")[0], 1.0 / item_size)
+                    else:
+                        match_item, rank_percent = recm_call_back(user, act_list, channel_list, org_list,
+                                                                  timestamp_list,
+                                                                  tag_list, timestamp, False)
+                        online_match_hr.add_sample(match_item, act.split("@")[0], 1.0/item_size)
+                        ids = np.argsort(-rank_percent)
+                        online_rank_hr.add_sample(match_item[ids], act.split("@")[0], 1.0 / item_size)
+
+            user_profile_db.push_act(user, act, channel, org, timestamp)
     hdfs_db.read_user_act_list(test_act_list_path, _eval_recm, exclude_act_type_list=exclude_act_type_list)
-    print("match------------------------------------------")
-    match_hr.print_eval()
-    print("rank-------------------------------------------")
-    rank_hr.print_eval()
+    print("match[default,offline,online]------------------------------------------")
+    default_match_hr.print_eval()
+    offline_match_hr.print_eval()
+    online_match_hr.print_eval()
+    print("rank[offline,online]-------------------------------------------")
+    offline_rank_hr.print_eval()
+    online_rank_hr.print_eval()
 
 
